f6afc1b6b848ccf80c50c6dc38e157a7e6563c2b
Alana Cheng <864636379@qq.com>
2019-03-13 09:41:52 +0800

    add variables

--- Practical 4/variables.py
+++ Practical 4/variables.py
@@ -11,3 +11,14 @@ b % 7
 c = b / 7
 d = c / 11
 e = d / 13
+print (e)
+
+X = True
+Y = True
+Z = (X and not Y) or (Y and not X)
+W = X != Y
+if (Z == W):
+    print ("True")
+else:
+    print ("False")
+
