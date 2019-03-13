ada33aa45a33038125167ed83887fc777f3e38cd
Alana Cheng <864636379@qq.com>
2019-03-13 09:08:09 +0800

    practical4

---
+++ Practical 4/collatz.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 08:46:00 2019
+
+@author: chenghui
+"""
+
---
+++ Practical 4/variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 08:32:36 2019
+
+@author: chenghui
+"""
+a = 425
+b = 425425
+b % 7
+c = b / 7
+d = c / 11
+e = d / 13
