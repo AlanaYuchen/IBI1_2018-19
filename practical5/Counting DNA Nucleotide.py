389d2d0061e9903430b94ebb597394e95e67de0d
Alana Cheng <864636379@qq.com>
2019-03-21 10:53:18 +0800

    improve counting DNA

--- practical5/Counting DNA Nucleotide.py
+++ practical5/Counting DNA Nucleotide.py
@@ -32,3 +32,26 @@ plt.pie(sizes, labels=labels,shadow=False, startangle=90, autopct='%1.1f%%')
 plt.title ('pie of the four DNA nucleotide')
 plt.axis('equal')
 plt.show()
+
+L= input ('give me a sequence of DNA:')
+A = 0
+T = 0
+G = 0
+C = 0
+for index in range (len(L)):
+    if L[index]=='A':
+        A = A+1
+    elif L[index]=='T':
+        T = T+1
+    elif L[index]=='G':
+        G = G+1
+    else:
+        C = C +1
+nt = len(L)
+import matplotlib.pyplot as plt
+labels = 'A', 'T', 'G', 'C'
+sizes = [percentA, percentT, percentG, percentC]
+plt.pie(sizes, labels=labels,shadow=False, startangle=90, autopct='%1.1f%%')
+plt.title ('pie of the four DNA nucleotide')
+plt.axis('equal')
+plt.show()
