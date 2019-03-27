885da2dbd0d6cb6e4e0608dc34b9e3fe16ab5686
Alana Cheng <864636379@qq.com>
2019-03-27 19:55:43 +0800

    some improvement

--- practical6/sending emails.py
+++ practical6/sending emails.py
@@ -6,26 +6,7 @@ Created on Tue Mar 26 22:18:11 2019
 @author: chenghui
 """
 
-email_address=[]
-namelist = []
-subjects = []
-emails = []
-import re
-email = open('address_information.csv', 'r') 
-for line in email:
-    lines = re.split(r',', str(line))
-    if re.search (r'@', lines[1])==None:
-        continue
-    email_address = lines[1]
-    p = re.compile(r"[^@]+@[^@]+\.[^@]+")
-    
-    if not p.match(email_address):
-           print (email_address,': Wrong Address')
-    else:
-           print (email_address,': Correct Address') 
-           namelist += [lines[0]]
-           subjects += [lines[2]]
-           emails +=[lines[1]]
+
            
 
 '''codes copied from website
@@ -61,20 +42,44 @@ try:
 except smtplib.SMTPException:
     print "Error: 无法发送邮件"
     '''
+email_address=[]
+namelist = []
+subjects = []
+emails = []
+import re
+email = open('address_information.csv', 'r') 
+for line in email:
+    lines = re.split(r',', str(line))
+    if re.search (r'@', lines[1])==None:
+        continue
+    email_address = lines[1]
+    p = re.compile(r"[^@]+@[^@]+\.[^@]+")
+    
+    if not p.match(email_address):
+           print (email_address,': Wrong Address')
+    else:
+           print (email_address,': Correct Address') 
+           namelist += [lines[0]]
+           subjects += [lines[2]]
+           emails +=[lines[1]]
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
-host = input('server:')
 user= input('user email address:')
 password = input('password:')
+host = 'smtp.'+ re.findall ('@(.+)',user)[0]
 mail_host= host
 mail_user= user   
 mail_pass=password
-text = 'Dear User,\nPlease find the results of your gene set linkage analysis result in attached file.\nThis is an email sent by the server, please don\'t reply.\nThank you for using GSLA.'
+text = open('body.txt','r')
+lines =''
+for line in text:
+    lines = lines + line
+print(lines)
 for i in range(0, len(namelist)):
     sender = user
     receivers = [emails[i]]
-    correct_message = re.sub(r'User',namelist[i],text)
+    correct_message = re.sub(r'User',namelist[i],lines)
     message = MIMEText(correct_message, 'plain','utf-8')          
     message['From'] = Header(user,'utf-8') 
     message['To'] = Header(subjects[i],'utf-8') 
@@ -83,15 +88,14 @@ for i in range(0, len(namelist)):
     
     try:
         smtpObj = smtplib.SMTP() 
-        print ("object created")
         smtpObj.connect(mail_host, 25)    
-        print ("server connected")
         smtpObj.login(mail_user,mail_pass)  
-        print ("server logged in")
         smtpObj.sendmail(sender, receivers, message.as_string())
         print ('Mail sent successfully')
     except smtplib.SMTPException:
         print ('Mail did not send successfully')
+
+
         
 # after a long term of debugging, it works finally!
 # for a lot of trial the errors always show that the email address cannot log in.
