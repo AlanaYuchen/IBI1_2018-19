57ff38e083ed04b89dbf5856c6dfe32f3d7513b9
Alana Cheng <864636379@qq.com>
2019-03-28 18:43:50 +0800

    add some notes to the code

--- practical6/sending emails.py
+++ practical6/sending emails.py
@@ -42,26 +42,31 @@ try:
 except smtplib.SMTPException:
     print "Error: 无法发送邮件"
     '''
+#-------------------Determine whether the email address is legal---------------
+# creat some empty list to store seperated information
 email_address=[]
 namelist = []
 subjects = []
 emails = []
+#seperate information to different lists
 import re
 email = open('address_information.csv', 'r') 
+#split the csv file by comma
 for line in email:
     lines = re.split(r',', str(line))
-    if re.search (r'@', lines[1])==None:
-        continue
+    if re.search (r'@', lines[1])==None: #check whether the information in line[1] is an email address
+        continue #skip the following codes and go to the next for loop directly if the line is not an email address
     email_address = lines[1]
-    p = re.compile(r"[^@]+@[^@]+\.[^@]+")
-    
-    if not p.match(email_address):
-           print (email_address,': Wrong Address')
+    p = re.compile(r"[^@]+@[^@]+\.[^@]+") #matching the correct format of an emial address
+    if not p.match(email_address): 
+           print (email_address,': Wrong Address!') 
     else:
-           print (email_address,': Correct Address') 
+           print (email_address,': Correct Address!') 
            namelist += [lines[0]]
            subjects += [lines[2]]
            emails +=[lines[1]]
+           
+#-------------------------sending the email---------------------------------           
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
@@ -69,7 +74,7 @@ user= input('user email address:')
 password = input('password:')
 host = 'smtp.'+ re.findall ('@(.+)',user)[0]
 mail_host= host
-mail_user= user   
+mail_user= user
 mail_pass=password
 text = open('body.txt','r')
 lines =''
@@ -82,24 +87,24 @@ for i in range(0, len(namelist)):
     correct_message = re.sub(r'User',namelist[i],lines)
     message = MIMEText(correct_message, 'plain','utf-8')          
     message['From'] = Header(user,'utf-8') 
-    message['To'] = Header(subjects[i],'utf-8') 
-    subject = subjects[i]
+    message['To'] = Header(subjects[i],'ascii') # it does not work if you use utf-8 here for information interchange.
+    subject = subjects[i]                       #askii: American Standard Code for Information Interchange
     message['Subject'] = Header(subject, 'utf-8')
     
     try:
         smtpObj = smtplib.SMTP() 
-        smtpObj.connect(mail_host, 25)    
-        smtpObj.login(mail_user,mail_pass)  
-        smtpObj.sendmail(sender, receivers, message.as_string())
+        smtpObj.connect(mail_host, 25)  #connect to the server  
+        smtpObj.login(mail_user,mail_pass) #login email acount  
+        smtpObj.sendmail(sender, receivers, message.as_string()) 
         print ('Mail sent successfully')
     except smtplib.SMTPException:
         print ('Mail did not send successfully')
 
 
-        
-# after a long term of debugging, it works finally!
-# for a lot of trial the errors always show that the email address cannot log in.
-
+# for a lot of trial the errors always show that the email address cannot log in. 
+# Sometimes if you try to send too many emails through a server, it might lock your account.
+#pay attention to the whitepace while writing loops!
+#adding 'print'function is a good method when debugging.
       
                
    
