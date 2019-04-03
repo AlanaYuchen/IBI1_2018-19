797122263db79bf6511eb6f992c1582b27354438
Alana Cheng <864636379@qq.com>
2019-04-03 09:11:31 +0800

    search code from internet

--- practical7/24point.py
+++ practical7/24point.py
@@ -6,23 +6,56 @@ Created on Wed Apr  3 00:22:48 2019
 @author: Alana
 """
 #----------------------input an integer between 1 and 23-----------------------
+
 import re
 t=0
 numbers = input ("Please input numbers to computer 24(use ',' to divide them):")
-numberlist = re.split(',',numbers)
-print(numberlist)
-numberlist = list(map(eval, numberlist))
-for i in numberlist:
+l = re.split(',',numbers)
+l = list(map(eval, l))
+for i in l:
     if (i%2!=0 and i%2!=1) or (i/2<=0.5 and i/2>12):
        print('The input number must be integers from 1 to 23')
        numbers = input ("Please input numbers to computer 24(use ',' to divide them):")
        t=t+1
        break
-    elif t==len(numberlist)-1:
+    elif t==len(l)-1:
         print ('Yes')
         break
     else:
         t=t+1
         continue
 
+
+#-----------------------calculate 24 point-------------------------------------
+
+from operator import mul, sub, add
+
+
+def div(a, b):
+    if b == 0:
+        return 999999.0
+    return a / b
+
+
+ops = {mul: '*', div: '/', sub: '-', add: '+'}
+
+def solve24(num, how, target):
+    if len(num) == 1:
+        if round(num[0], 5) == round(target, 5):
+            yield str(how[0]).replace(',', '').replace("'", '')
+    else:
+        for i, n1 in enumerate(num):
+            for j, n2 in enumerate(num):
+                if i != j:
+                    for op in ops:
+                        new_num = [n for k, n in enumerate(num) if k != i and k != j] + [op(n1, n2)]
+                        new_how = [h for k, h in enumerate(how) if k != i and k != j] + [(how[i], ops[op], how[j])]
+                        yield from solve24(new_num, new_how, target)
+
+for nums in l:
+    print(nums, end=' : ')
+    try:
+        print(next(solve24(nums, nums, 24)))
+    except StopIteration:
+        print("No solution found")
        
